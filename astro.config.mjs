6684170d5ca3ad20ea5cf937c3773b75ac6b672d
tmghna <tamaghnadey1916@gmail.com>
2026-03-27 16:19:11 +0530

    added cms (sanity) and updated astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,19 @@
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import sanity from '@sanity/astro';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [
+		mdx(),
+		sitemap(),
+		sanity({
+			projectId: '74ymsp5l', // Replace with your Sanity project ID
+			dataset: 'production',
+			useCdn: true, // Use CDN for faster reads (set to false for fresh data)
+		}),
+	],
 });
